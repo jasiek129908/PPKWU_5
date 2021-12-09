4aa4079e37f0e427f0f20b2077551bc6c85abe24
Jan Oleksik <222529@edu.p.lodz.pl>
2021-12-09 14:07:15 +0100

    added generating vcf card

--- src/main/java/com/example/ppkwu_5/Controller.java
+++ src/main/java/com/example/ppkwu_5/Controller.java
@@ -1,8 +1,6 @@
 package com.example.ppkwu_5;
 
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
 
@@ -15,7 +13,7 @@ public class Controller {
     public String getBusinessCard(@PathVariable String profession) {
         String response = null;
         try {
-            response = Parse.parseResponse(EXTERNAL_API + "/" + profession);
+            response = PPKWU_5.parseResponse(EXTERNAL_API + "/" + profession);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -27,14 +25,7 @@ public class Controller {
                                    @RequestParam String email, @RequestParam String website,
                                    @RequestParam String street, @RequestParam String postalCode,
                                    @RequestParam String addressLocality) {
-        System.out.println(name);
-        System.out.println(telephone);
-        System.out.println(email);
-        System.out.println(website);
-        System.out.println(street);
-        System.out.println(postalCode);
-        System.out.println(addressLocality);
-        return "";
+        return PPKWU_5.generateCard(name, telephone, email, website, street, postalCode, addressLocality);
     }
 }
 
--- src/main/java/com/example/ppkwu_5/PPKWU_5.java
+++ src/main/java/com/example/ppkwu_5/PPKWU_5.java
@@ -1,6 +1,10 @@
 package com.example.ppkwu_5;
 
 
+import ezvcard.Ezvcard;
+import ezvcard.VCard;
+import ezvcard.VCardVersion;
+import ezvcard.property.Address;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
@@ -10,7 +14,7 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
-public class Parse {
+public class PPKWU_5 {
 
     public static String parseResponse(String profession) throws IOException {
         String htmlResponse = "<!doctype html>\n" +
@@ -50,30 +54,44 @@ public class Parse {
                 addressLocality = jsonObject2.getString("addressLocality");
                 postalCode = jsonObject2.getString("postalCode");
                 country = jsonObject2.getString("addressCountry");
-            }catch (JSONException e){
+            } catch (JSONException e) {
                 System.out.println(e);
             }
             String paramToApi = String.format("name=%s&telephone=%s&email=%s&website=%s&street=%s&postalCode=%s&addressLocality=%s",
                     name, telephone, email, website, street, postalCode, addressLocality);
 
             htmlResponse += String.format("<div>\n" +
-                    "    <p>%s</p>\n" +
-                    "    <p><img src=\"%s\" alt=\"componay image\"></img>\n" +
-                    "    <p>telephone: %s</p>\n" +
-                    "    <p>Email: %s</p>\n" +
-                    "    <p>Website: %s</p>\n" +
-                    "  \n" +
-                    "    <p>Address</p>" +
-                    "    <p>%s</p>\n" +
-                    "    <p>%s</p>\n" +
-                    "    <p>%s</p>\n" +
-                    "    <p>%s</p>\n" +
-                    "    <a href=\"/api/card/?%s>\"><button>wygeneruj vCard</button></a>\n"+
-                    "</div><br>",
+                            "    <p>%s</p>\n" +
+                            "    <p><img src=\"%s\" alt=\"componay image\"></img>\n" +
+                            "    <p>telephone: %s</p>\n" +
+                            "    <p>Email: %s</p>\n" +
+                            "    <p>Website: %s</p>\n" +
+                            "  \n" +
+                            "    <p>Address</p>" +
+                            "    <p>%s</p>\n" +
+                            "    <p>%s</p>\n" +
+                            "    <p>%s</p>\n" +
+                            "    <p>%s</p>\n" +
+                            "    <a href=\"/api/card/?%s>\"><button>wygeneruj vCard</button></a>\n" +
+                            "</div><br>",
                     name, image, telephone, email, website, street, postalCode, addressLocality, country, paramToApi);
         }
         htmlResponse += "</body></html>";
         return htmlResponse;
+    }
+
+    public static String generateCard(String name, String telephone, String email, String website, String street, String postalCode, String addressLocality) {
+        VCard vcard = new VCard();
+        Address address = new Address();
+        address.setPostalCode(postalCode);
+        address.setStreetAddress(street);
+        address.setRegion(addressLocality);
+        vcard.setFormattedName(name);
+        vcard.addTelephoneNumber(telephone);
+        vcard.addEmail(email);
+        vcard.addUrl(website);
+        vcard.addAddress(address);
+        return Ezvcard.write(vcard).version(VCardVersion.V4_0).go();
     }
 
 }
